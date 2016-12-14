3464b8bdf557f43247a6caf1b9a430220b6ada4c
James Fraser <wulfgar.pro@gmail.com>
2016-12-14 21:13:12 +1100

    finished test example pass 1

--- testing/src/test/java/AppTest.java
+++ testing/src/test/java/AppTest.java
@@ -5,11 +5,13 @@ import static org.junit.Assert.*;
 public class AppTest {
     private App app;
 
+    @Before
     public void before() {
         app = new App();
     }
 
+    @Test
     public void adding_two_numbers_does_sum() {
-        assertTrue(2 == app.sum(1,1));
+        assertTrue(3 == app.sum(1,1));
     }
 }
